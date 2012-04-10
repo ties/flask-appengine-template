3075940a5babad6122cdd375827c5d781227595b
Ties de Kock <ties@tiesdekock.nl>
2012-04-11 00:49:44 +0200

    Compiled templates support, revision in footer

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -2,11 +2,25 @@
 Initialize Flask app
 
 """
+import logging, os
 
 from flask import Flask
+from jinja2 import ModuleLoader
 from gae_mini_profiler import profiler
 
-app = Flask('application')
+# Use compiled templates as seen in <https://github.com/mitsuhiko/flask/issues/101>
+class GAEFlask(Flask):
+	def create_global_jinja_loader(self):
+		if self.debug:
+			return super(GAEFlask, self).create_global_jinja_loader()
+		else:
+			filename = 'compiled_templates.zip'
+			logging.info("Loading templates from {}".format(filename))
+			if not os.path.isfile(filename):
+				raise TemplateNotFound("Missing {}".format(filename))
+			return ModuleLoader(filename)
+
+app = GAEFlask('application')
 app.config.from_object('application.settings')
 
 def get_request_id():
